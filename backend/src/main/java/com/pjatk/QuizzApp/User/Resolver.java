1ad0f6e47562466fd1a0a987f7262e94f231b120
Witek248 <szywit248@gmail.com>
2025-06-16 17:37:00 +0200

    rebase

--- backend/src/main/java/com/pjatk/QuizzApp/User/Resolver.java
+++ backend/src/main/java/com/pjatk/QuizzApp/User/Resolver.java
@@ -1,29 +1,29 @@
-package com.pjatk.QuizzApp.User;
-
-import com.pjatk.QuizzApp.Exceptions.UserNotFoundException;
-import lombok.RequiredArgsConstructor;
-import org.springframework.graphql.data.method.annotation.Argument;
-import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.stereotype.Component;
-
-import java.util.List;
-
-@RequiredArgsConstructor
-@Component
-public class Resolver
-{
-    private final UserRepository userRepository;
-
-    @QueryMapping
-    public User user(@Argument Integer id)
-    {
-        return userRepository.findById(id)
-                .orElseThrow(()->new UserNotFoundException("User with id:"+id+" not found"));
-    }
-
-    @QueryMapping
-    public List<User> users()
-    {
-        return userRepository.findAll();
-    }
-}
+//package com.pjatk.QuizzApp.User;
+//
+//import com.pjatk.QuizzApp.Exceptions.UserNotFoundException;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.graphql.data.method.annotation.Argument;
+//import org.springframework.graphql.data.method.annotation.QueryMapping;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.List;
+//
+//@RequiredArgsConstructor
+//@Component
+//public class Resolver
+//{
+//    private final UserRepository userRepository;
+//
+//    @QueryMapping
+//    public User user(@Argument Integer id)
+//    {
+//        return userRepository.findById(id)
+//                .orElseThrow(()->new UserNotFoundException("User with id:"+id+" not found"));
+//    }
+//
+//    @QueryMapping
+//    public List<User> users()
+//    {
+//        return userRepository.findAll();
+//    }
+//}
